8cc78972dcc09eaa4b46934b10d4b5f1c3855d53
Jackie Mok <jackiedmok@gmail.com>
2016-10-31 18:49:26 -0400

    updated with better spacing and commenting

--- Numerical Linear Algebra/LU_nopivot.m
+++ Numerical Linear Algebra/LU_nopivot.m
@@ -1,11 +1,11 @@
 %% LU Solve without Pivoting
-% (A = LU) => Ax = b <=> (LU)x = b
+% ( A = LU ) => ( Ax = b <=> (LU)x = b )
 
 % Input:    A - (n x n) matrix
-%           b - n-length vector
+%           b - n-length vector equivalent to A*xsol
 %           xsol - [Optional input] The exact solution used to obtain b
-% Output:   L - Lower factor of A
-%           U - Upper factor of A
+% Output:   L - (n x n) lower factor of A
+%           U - (n x n) upper factor of A
 %           x - The computed solution via forward & backward substitutions
 
 function [ L, U, x ] = LU_nopivot( A, b, xsol )
@@ -16,10 +16,11 @@ if m ~= n
     return
 end
 
-% Initialize LU factors of A
+% Initialize L & U factors of A
 L = eye(n);
 U = A; 
 
+% Gaussian Elimination without Pivoting
 for k = 1:n-1
     for j = k+1:n
         
@@ -29,6 +30,7 @@ for k = 1:n-1
             return
         end
         
+        % Compute multiplier for row j
         L(j,k) = U(j,k) / U(k,k);
         
         % Row j <-- Row j - multiplier * (pivot row)
@@ -37,22 +39,24 @@ for k = 1:n-1
 end 
 
 %% Solve the system
+
 % Forward substitution for intermediate solution: Ly = b
-% (A = LU) => Ax = b <=> (LU)x = b
+% ( A = LU ) => ( Ax = b <=> (LU)x = b )
 % <=> L(Ux) = b, where y = (Ux)
 y = L \ b;  
 % Backward substitution for final solution: Ux = y
 x = U \ y; 
 
-
 %% Analysis
 % Compute the residual
 r = b - A*x;
 
+% Print relative residual norm
 rep = sprintf( 'The relative residual norm is: %0.5g', ...
     norm(r) / norm(b) );
 disp(rep)
 
+% Print relative accuracy
 if nargin == 3
     rep = sprintf( 'The relative accuracy is: %0.5g', ...
         norm(xsol - x) / norm(xsol) );
