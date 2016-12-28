c6fdd69473ebebe3a04cae8eb7f348b7d65ae647
Jackie Mok <jackiedmok@gmail.com>
2016-12-28 15:39:03 -0500

    better comments

--- Probability/bin_poiss.m
+++ Probability/bin_poiss.m
@@ -1,4 +1,4 @@
-%% Binomial & Poisson Distributions
+%% Binomial & Poisson (Approximation) Distributions
 % Given n independent trials and a probability of success p,
 % compute the probability that we observe k = 0,1,...,n successes
 % in n independent trials.
@@ -7,10 +7,10 @@
 % => Random variable X = (X_1 + ... + X_n) ~ Bin(n,p)
 % For large n and small p, X is approximately Poisson(n*p)-distributed
 
-% Input:                    n - The number of independent trials
+% Input:                    n - The integer number of independent trials
 %                           p - P(X_i = 1); i.e., the probability of success for any given trial
-%                           k - The number of desired successes
-% Output:                   mean - Expected value or mean of random variable X
+%                           k - The integer number of desired successes
+% Output:                   mean - Expected value (mean) of random variable X
 %                           var - Variance of random variable variable X
 %                           bin_pk - P(X = k) if X ~ Bin(n,p)
 %                           poiss_pk - P(X = k) if X ~ Poiss(n*p)
@@ -24,16 +24,20 @@ function [ mean, var, bin_pk, poiss_pk, bin_ck, poiss_ck, error_pk, error_ck ] =
 % Compute expectation E[X] = mu = lambda (Poisson parameter)
 mean = n * p;
 
-% Compute variance sigma^2 = n*p(1 - p)
+% Compute variance sigma^2 = n*p*(1 - p)
 var = mean * (1 - p);
 
-% Compute probability assuming X ~ Bin(n,p)
+%% PDF
+
+% Compute probability of exactly k successes, assuming X ~ Bin(n,p)
 bin_pk = factorial(n) / (factorial(k) * factorial(n - k)) * (p^k) * (1 - p)^(n - k);
 
-% Compute probability assuming X ~ Poisson(np)
+% Compute probability of exactly k successes, assuming X ~ Poisson(np)
 poiss_pk = (mean^k) * exp(-mean) / factorial(k);
 
-% Compute P(X <= k)
+%% CDF
+
+% Compute P(X <= k); i.e., compute the probability of k or fewer successes
 % Initialize bin_ck, poiss_ck
 bin_ck = 0; poiss_ck = 0;
 for i = 0:k
