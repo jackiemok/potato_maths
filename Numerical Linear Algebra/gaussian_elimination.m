31da840685ceacbb97bf3025077b7f5cee523b70
Jackie Mok <jackiedmok@gmail.com>
2017-01-01 03:30:29 -0500

    simplify loops and edit comments

--- Numerical Linear Algebra/gaussian_elimination.m
+++ Numerical Linear Algebra/gaussian_elimination.m
@@ -3,7 +3,7 @@
 
 % Input         A - (n x n) matrix
 %               b - (n x 1) vector equivalent to A*x
-% Output        x - (n x 1) vector such that Ax = b
+% Output        x - (n x 1) vector such that A*x = b
 
 function [ x ] = gaussian_elimination( A, b )
 
@@ -13,26 +13,17 @@ x = zeros(n,1);
 
 %% Gaussian Elimination without Pivoting
 
-for k = 1:n-1                     % Loop over columns
-    for i = k+1:1:n               % Loop over rows
-        
-        multiplier = A(i,k) / A(k,k);                 
-        
-        for j = k+1:1:n         
-            A(i,j) = A(i,j) - multiplier * A(k,j);       
-        end
-        
+for k = 1:(n - 1)                 % Loop over columns
+    for i = (k + 1):n             % Loop over rows
+        multiplier = A(i,k) / A(k,k);                        
+        A(i,k+1:n) = A(i,k+1:n) - multiplier * A(k,k+1:n);       
         b(i) = b(i) - multiplier * b(k);                      
     end 
-
 end
 
 %% Back Substitution
 
 for i = n:-1:1    
-    for j = i+1:n
-        b(i) = b(i) - A(i,j) * x(j);                    
-    end
-    
+    b(i) = b(i) - A(i,i+1:n) * x(i+1:n);                    
     x(i) = b(i) / A(i,i);                       
 end
